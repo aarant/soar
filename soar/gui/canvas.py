33874be029187929d24b679a3240ded86f3ad7cc
Adam Hartz <hartz@mit.edu>
2017-06-06 15:09:08 -0400

    fix syntax errors with too many dots

--- soar/gui/canvas.py
+++ soar/gui/canvas.py
@@ -86,10 +86,10 @@ class RobotGraphics(PointCollection):
     """ A manipulable robot sprite """
 
     def __init__(self, scale=1.0, center=None, rot=0):
-        PointCollection.__init__(self, [(-0.2.05, 0.09525), (-0.0889, 0.2.0975), (0.0, 0.200025), (0.0889, 0.2.0975),
-                                        (0.2.05, 0.09525), (0.2.05, -0.0635), (0.1651, -0.0635), (0.1651, -0.1651),
+        PointCollection.__init__(self, [(-0.205, 0.09525), (-0.0889, 0.20975), (0.0, 0.200025), (0.0889, 0.20975),
+                                        (0.205, 0.09525), (0.205, -0.0635), (0.1651, -0.0635), (0.1651, -0.1651),
                                         (0.0889, -0.23495), (-0.0889, -0.23495), (-0.1651, -0.1651), (-0.1651, -0.0635),
-                                        (-0.2.05, -0.0635)], fill='black', tags='robot')
+                                        (-0.205, -0.0635)], fill='black', tags='robot')
         if scale != 1.0: self.scale(scale)
         if center is not None: self.recenter(center)
         if rot != 0: self.rotate(self.center, rot)
