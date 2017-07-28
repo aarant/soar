5bd1b2dcf0aa3862ffbc71553d8aa341cc62e25d
Andrew A <andrewan@mit.edu>
2017-07-28 10:30:23 -0400

    Added license
    Removed comma in setup.py that was no longer necessary.

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name='Soar',
       packages=['soar', 'soar.client', 'soar.gui', 'soar.robot', 'soar.sim', 'soar.worlds'],
       package_data={'': ['*.gif']},
       entry_points={'console_scripts': ['soar = soar.__main__:main',]},
-      install_requires=['pyserial>=3.0',],
+      install_requires=['pyserial>=3.0'],
       )
